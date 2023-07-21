12094e21b3a1921e33a19c4a482651c5703497ed
Justin A Perry <jadamsperry@gmail.com>
2023-07-20 18:54:59 -0600

    upload for staging test

--- schedule/tests.py
+++ schedule/tests.py
@@ -1,146 +1,139 @@
-from django.test import TestCase, Client
-from django.urls import reverse
-from .forms import ContactForm
-from unittest.mock import patch
-
-# Create your tests here.
+from django.test import TestCase
 from .models import Music_artist_listing, Visual_artist_listing, Extra_curriucular_listing, Nearby_accomodations, Heads_up_music
 
-
-class MusicArtistListingTestCase(TestCase):
+class MusicArtistListingTest(TestCase):
     def setUp(self):
         self.artist = Music_artist_listing.objects.create(
             artist_name='John Doe',
             artist_genre='Rap',
-            show_date='2023-07-01',
-            show_time='10:10',
-            artist_bio='Lorem ipsum dolor sit amet',
-            artist_insta='john.doe',
-            artist_website='https://example.com',
-            artist_music_page='https://musicpage.com',
-            entry_price=20.5
+            show_date='2023-07-25',
+            show_time='18:00:00',
+            artist_bio='Talented rapper',
+            entry_price=15.99,
+            image_url='https://example.com/image.jpg'
         )
 
     def test_artist_name(self):
         self.assertEqual(self.artist.artist_name, 'John Doe')
 
+    def test_artist_genre(self):
+        self.assertEqual(self.artist.artist_genre, 'Rap')
 
-class VisualArtistListingTestCase(TestCase):
+    def test_show_date(self):
+        self.assertEqual(str(self.artist.show_date), '2023-07-25')
+
+    def test_show_time(self):
+        self.assertEqual(str(self.artist.show_time), '18:00:00')
+
+    def test_artist_bio(self):
+        self.assertEqual(self.artist.artist_bio, 'Talented rapper')
+
+    def test_entry_price(self):
+        self.assertEqual(self.artist.entry_price, 15.99)
+
+    def test_image_url(self):
+        self.assertEqual(self.artist.image_url, 'https://example.com/image.jpg')
+
+
+class VisualArtistListingTest(TestCase):
     def setUp(self):
-        self.visual_artist = Visual_artist_listing.objects.create(
+        self.artist = Visual_artist_listing.objects.create(
             visual_show_name='Art Show',
             artist_name='Jane Smith',
-            artist_medium='Painting',
-            show_date_start='2023-07-01',
-            show_date_end='2023-07-10',
-            artist_bio='Lorem ipsum dolor sit amet',
-            artist_website='https://example.com',
-            entry_price=10.99,
-            age_restriction=False
+            artist_medium='Oil painting',
+            show_date_start='2023-07-20',
+            show_date_end='2023-07-30',
+            artist_bio='Talented visual artist',
+            entry_price=10.50,
+            age_restriction=False,
+            image_url='https://example.com/art_image.jpg'
         )
 
     def test_visual_show_name(self):
-        self.assertEqual(self.visual_artist.visual_show_name, 'Art Show')
+        self.assertEqual(self.artist.visual_show_name, 'Art Show')
+
+    def test_artist_name(self):
+        self.assertEqual(self.artist.artist_name, 'Jane Smith')
 
 
-class ExtraCurricularListingTestCase(TestCase):
+
+
+
+# views
+
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Music_artist_listing, Visual_artist_listing, Extra_curriucular_listing, Nearby_accomodations
+from .forms import ContactForm
+
+class ContactUsViewTest(TestCase):
     def setUp(self):
-        self.extra_curricular = Extra_curriucular_listing.objects.create(
-            class_name='Dance Class',
-            class_teacher='John Smith',
-            class_description='Lorem ipsum dolor sit amet',
-            class_date='2023-07-01',
-            class_time='10:00',
-            class_location='Revolt Annex',
-            class_price=15.99
-        )
+        self.client = Client()
+        self.url = reverse('contact_us')
 
-    def test_class_name(self):
-        self.assertEqual(self.extra_curricular.class_name, 'Dance Class')
+    def test_contact_us_view_post(self):
+        response = self.client.post(self.url, {
+            'name': 'John Doe',
+            'email': 'john@example.com',
+            'subject': 'Hello',
+            'message': 'This is a test message.',
+        })
+        self.assertEqual(response.status_code, 302)  # Expecting a redirect
 
+        # Check if the email was sent successfully
+        self.assertContains(response, 'email sent')
 
-class NearbyAccommodationsTestCase(TestCase):
+    def test_contact_us_view_invalid_header(self):
+        response = self.client.post(self.url, {
+            'name': 'John Doe',
+            'email': 'john@example.com',
+            'subject': 'Subject with\nnewline',
+            'message': 'This is a test message.',
+        })
+        self.assertEqual(response.status_code, 200)  # Expecting a response with status code 200
+        self.assertContains(response, 'Invalid header found.')
+
+class AnnexHomeViewTest(TestCase):
     def setUp(self):
-        self.accommodation = Nearby_accomodations.objects.create(
-            accom_name='Hotel ABC',
-            accom_type='Hotel',
-            accom_distance=1.5,
-            accom_phone='1234567890',
-            accom_url='https://example.com/hotel'
-        )
+        self.client = Client()
+        self.url = reverse('annex_home')
 
-    def test_accom_name(self):
-        self.assertEqual(self.accommodation.accom_name, 'Hotel ABC')
-
-
-class HeadsUpMusicTestCase(TestCase):
-    def setUp(self):
-        self.music_event = Heads_up_music.objects.create(
-            event_name='Concert',
-            event_date='2023-07-01',
-            event_time='19:30',
+        # Create some test data for Music_artist_listing and Visual_artist_listing models
+        Music_artist_listing.objects.create(
             artist_name='John Doe',
-            event_description='Lorem ipsum dolor sit amet'
+            artist_genre='Rap',
+            show_date='2023-07-25',
+            show_time='18:00:00',
+            artist_bio='Talented rapper',
+            entry_price=15.99,
+            image_url='https://example.com/image.jpg'
         )
 
-    def test_event_name(self):
-        self.assertEqual(self.music_event.event_name, 'Concert')
-
-
-
-class ContactUsTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    @patch('django.core.mail.send_mail')
-    def test_contact_us_post(self, mock_send_mail):
-        form_data = {
-            'name': 'John Doe',
-            'email': 'john.doe@example.com',
-            'subject': 'Test Subject',
-            'message': 'Test message',
-        }
-        response = self.client.post(reverse('contact_us'), data=form_data)
-        self.assertEqual(response.status_code, 302)  # Assuming a successful redirect
-        self.assertRedirects(response, reverse('annex_home'))
-        mock_send_mail.assert_called_once_with(
-            form_data['subject'],
-            '\n'.join(form_data.values()),
-            'admin@example.com',
-            ['admin@example.com']
+        Visual_artist_listing.objects.create(
+            visual_show_name='Art Show',
+            artist_name='Jane Smith',
+            artist_medium='Oil painting',
+            show_date_start='2023-07-20',
+            show_date_end='2023-07-30',
+            artist_bio='Talented visual artist',
+            entry_price=10.50,
+            age_restriction=False,
+            image_url='https://example.com/art_image.jpg'
         )
 
-    def test_contact_us_invalid_header(self):
-        form_data = {
-            'name': 'John Doe',
-            'email': 'john.doe@example.com',
-            'subject': 'Test Subject',
-            'message': 'Test message',
-            # Invalid header field, may result in BadHeaderError
-            'subject\nfield': 'Invalid header',
-        }
-        response = self.client.post(reverse('contact_us'), data=form_data)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content.decode(), 'Invalid header found.')
+    def test_annex_home_view(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)  # Expecting a response with status code 200
 
+        # Check if the context data contains the required objects
+        self.assertTrue('show_listing' in response.context)
+        self.assertTrue('gallery_listing' in response.context)
+        self.assertTrue('extra_curriucular_listing' in response.context)
+        self.assertTrue('bars_nearby' in response.context)
+        self.assertTrue('place_image_list' in response.context)
+        self.assertTrue('contact_form' in response.context)
+        self.assertTrue('api_key' in response.context)
 
-class AnnexHomeTestCase(TestCase):
-    def setUp(self):
-        self.client = Client()
-
-    def test_annex_home_get(self):
-        response = self.client.get(reverse('annex_home'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'master-new.html')
-
-
-# Additional test cases for other view functions can be added similarly
-
-
-
-
-
-
-
-
-
+        # Check if the rendered page contains certain content based on the data we created in the setUp method
+        self.assertContains(response, 'John Doe')
+        self.assertContains(response, 'Art Show')
