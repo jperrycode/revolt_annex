707cd172642b7d1e032a27c4e10c53ef5da2902e
taoshaus <jadamsperry@gmail.com>
2023-08-17 14:07:04 -0600

    update
    chaging front end nav and sectional UI relationships

--- schedule/apps.py
+++ schedule/apps.py
@@ -1,15 +1,14 @@
 from django.apps import AppConfig
 
-
-
 class ArchiveConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'schedule'
     verbose_name = 'Art and Music Schedule'
 
     def ready(self):
-        # Import signal handlers
-        import schedule.signals
+        # Import signal handlers from your app
+        import schedule.signals  # Replace "schedule" with your app's name
+
 
 # archive/__init__.py
 default_app_config = 'archive.apps.ArchiveConfig'
