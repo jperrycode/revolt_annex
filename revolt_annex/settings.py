2570dec0f0d1e6abf370b9fe8143fc3ab80a2db5
taoshaus <jadamsperry@gmail.com>
2023-09-13 03:27:01 -0600

    auth

--- revolt_annex/settings.py
+++ revolt_annex/settings.py
@@ -48,7 +48,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # DEBUG = False
 # DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 DEBUG = False
-ALLOWED_HOSTS = ['3.138.33.133','127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['3.138.33.133','127.0.0.1', 'localhost', '*.herokuapp.com']
 
 
 # Application definition
