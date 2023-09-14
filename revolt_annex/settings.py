03f7198c107d97a308aa70eb71135c9f3ca19a58
taoshaus <jadamsperry@gmail.com>
2023-09-13 23:09:34 -0600

    smtp

--- revolt_annex/settings.py
+++ revolt_annex/settings.py
@@ -193,11 +193,13 @@ CELERY_BEAT_SCHEDULER = 'celery.beat.PersistentScheduler'
 
 # SMTP SETTINGS
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_USE_TLS = True
-EMAIL_HOST = "smtp.sendgrid.net"
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "apikey"
-EMAIL_HOST_PASSWORD = str(os.getenv('SENDGRID_KEY'))
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'taosrevolt@gmail.com'
 
 
