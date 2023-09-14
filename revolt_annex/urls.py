e446c17e3b03b4cd52b46e45bfe0b0ccb69bdd20
taoshaus <jadamsperry@gmail.com>
2023-09-14 16:57:30 -0600

    urls

--- revolt_annex/urls.py
+++ revolt_annex/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('schedule.urls')),
-    path("__reload__/", include("django_browser_reload.urls")),
-    path("__debug__/", include("debug_toolbar.urls")),
+    # path("__reload__/", include("django_browser_reload.urls")),
+    # path("__debug__/", include("debug_toolbar.urls")),
     
 ]
 
