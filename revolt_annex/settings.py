894e69d03e960e4c0b7c78cfbfc4770a7836af58
taoshaus <jadamsperry@gmail.com>
2023-09-14 01:19:18 -0600

    update port

--- revolt_annex/settings.py
+++ revolt_annex/settings.py
@@ -198,7 +198,7 @@ SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'taosrevolt@gmail.com'
 
