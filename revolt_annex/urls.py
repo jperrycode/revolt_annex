0fa7afeb213fb6d8215e79e44700054bdc689be0
taoshaus <jadamsperry@gmail.com>
2023-09-16 14:11:10 -0600

    url

--- revolt_annex/urls.py
+++ revolt_annex/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('schedule.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
-    path("__debug__/", include("debug_toolbar.urls")),
+    
     
 ]
 
