e1d96e2b71b8468cfabafa5546e16b2d53fd4a27
taoshaus <jadamsperry@gmail.com>
2023-09-16 14:09:25 -0600

    comit for mobile

--- revolt_annex/urls.py
+++ revolt_annex/urls.py
@@ -28,5 +28,11 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
+
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL,
                           document_root=settings.STATIC_ROOT)
