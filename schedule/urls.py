b664e350ad2d56fb6d2f289cda6d489566b32f61
taoshaus <jadamsperry@gmail.com>
2023-10-03 05:23:48 -0600

    templateview

--- schedule/urls.py
+++ schedule/urls.py
@@ -3,15 +3,16 @@ from . import views
 from .views import *
 from django.views.generic import RedirectView
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
     path('', RedirectView.as_view(url='revolt-art/')),
-    path('revolt-art/', AnnexHomeView.as_view(), name='annex_home'),
+    path('revolt-art/', TemplateView.as_view(template_name = 'schedule/master-new.html'), name='annex_home'),
     # path('revolt-test/', AnnexTestView.as_view(), name='annex_test'),
     path('contact_us/', ContactUsView.as_view(), name='contact_us'),
     # path('art-archive/', ArchiveView.as_view(), name='archive-home'),
-   path('contact_us_success', ContactSuccessView.as_view(), name='contact_success'),
+   path('contact_us_success', views.ContactSuccessView, name='contact_success'),
 ]
 
 admin.site.site_header = 'Revolt Gallery - Reset Performing Arts'
--- schedule/views.py
+++ schedule/views.py
@@ -141,7 +141,7 @@ class ContactSuccessView(TemplateResponse):
 
 
 class AnnexHomeView(TemplateView):
-    template_name = 'schedule/master-new.html'
+    # template_name = 'schedule/master-new.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
